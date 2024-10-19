1826a5f7a352d989f6bffea0aaaa33985c4ab8d5
Guy Luong <guy.luong.7@gmail.com>
2024-10-19 22:25:31 +0200

    [UI] Added a page of Pokemon in Home

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,12 +1,15 @@
+import useFetch from "../Hooks/useFetch";
+
 import PokemonCard from "../Components/PokemonCard";
 
 const Home = () => {
+    const { data, error } = useFetch("https://pokeapi.co/api/v2/pokemon?page=1");
+
     return (
         <div>
-            <PokemonCard name="ditto"/>
-            <PokemonCard name="pikachu"/>
+            {data.results && data.results.map((r, i) => <PokemonCard key={i} name={r.name} />)}
         </div>
     );
 }
- 
+
 export default Home;
