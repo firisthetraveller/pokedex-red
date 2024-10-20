8ee7f37e2d47ffe23eb12137790095ac29b30a67
Guy <guy.luong.7@gmail.com>
2024-10-20 15:30:17 +0200

    [Dev] Use PokemonId context

--- src/App.js
+++ src/App.js
@@ -7,22 +7,25 @@ import './App.css';
 import './output.css';
 import Home from './Pages/Home';
 import PokemonPage from './Pages/PokemonPage';
+import { PokemonIdProvider } from './Contexts/PokemonIdContext';
 
 function App() {
   return (
     <div>
-      <BrowserRouter basename='/pokedex-red'>
-        <Navbar />
-        <div className='min-h-16' />
-        <div>
-          <Routes>
-            <Route index element={<Navigate to="/home" replace />} />
-            <Route path="home" element={<Home />} />
-            <Route path="pokemon/:name" element={<PokemonPage />} />
-            <Route path="*" element={<Navigate to="/home" />} />
-          </Routes>
-        </div>
-      </BrowserRouter>
+      <PokemonIdProvider>
+        <BrowserRouter basename='/pokedex-red'>
+          <Navbar />
+          <div className='min-h-16' />
+          <div>
+            <Routes>
+              <Route index element={<Navigate to="/home" replace />} />
+              <Route path="home" element={<Home />} />
+              <Route path="pokemon/:name" element={<PokemonPage />} />
+              <Route path="*" element={<Navigate to="/home" />} />
+            </Routes>
+          </div>
+        </BrowserRouter>
+      </PokemonIdProvider>
     </div>
   );
 }
--- src/Components/PokemonCard.js
+++ src/Components/PokemonCard.js
@@ -1,14 +1,16 @@
 import { Link } from "react-router-dom";
 
 import useFormat from "../Hooks/useFormat";
+import usePokemonIds from "../Hooks/usePokemonIds";
 
 const PokemonCard = ({ id, name }) => {
     const { capitalize } = useFormat();
+    const { getImageFromName } = usePokemonIds();
 
     return (
         <div className="p-5">
             <Link to={`/pokemon/${name}`}>
-                <img src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${id}.png`} />
+                <img src={getImageFromName(name)} />
                 #{id} {capitalize(name)}
             </Link>
         </div>
