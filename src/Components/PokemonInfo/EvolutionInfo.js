3ccac30b8659fbf8aaa83f3a544996b8d753b16f
Guy Luong <guy.luong.7@gmail.com>
2024-10-21 17:56:53 +0200

    [UI] Removed parenthesis

--- src/Components/PokemonInfo/EvolutionInfo.js
+++ src/Components/PokemonInfo/EvolutionInfo.js
@@ -22,9 +22,9 @@ const EvolutionInfo = ({ data }) => {
                             {data.evolution_details.map((d, i) =>
                                 <React.Fragment key={i}>
                                     <div className="flex flex-col items-center">
-                                        {d.min_level && <span>(Level {data.evolution_details[0].min_level})</span>}
+                                        {d.min_level && <span>Lvl. {data.evolution_details[0].min_level}</span>}
                                         {d.min_happiness &&
-                                            <span>(Happy{d.time_of_day ? ` at ${d.time_of_day}` : ""}{d.known_move_type ? <span> with a {<TypeInfo name={d.known_move_type.name} />} move</span> : ""})</span>}
+                                            <span>Happy{d.time_of_day ? ` at ${d.time_of_day}` : ""}{d.known_move_type ? <span> with a {<TypeInfo name={d.known_move_type.name} />} move</span> : ""}</span>}
                                         {d.item && <>
                                             <img src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/items/${d.item.name}.png`} alt={`Sprite for ${capitalizeAllString(d.item.name)}`} />
                                             <p>{capitalizeAllString(d.item.name)}</p>
