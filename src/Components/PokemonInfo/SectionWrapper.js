8f9d760cab3e9ed92f9e9014293e3f639c08b29e
Guy Luong <guy.luong.7@gmail.com>
2024-10-21 18:30:16 +0200

    [CSS] Add y-margin

--- src/Components/PokemonInfo/SectionWrapper.js
+++ src/Components/PokemonInfo/SectionWrapper.js
@@ -2,7 +2,7 @@ import Heading from "../Base/Heading";
 
 const SectionWrapper = ({ children, name }) => {
     return (
-        <div>
+        <div className="my-2">
             <Heading level={2}>{name}</Heading>
             {children}
         </div>
