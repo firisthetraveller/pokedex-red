0532848bbfc8111060b3db20573d3295dd724361
Guy Luong <guy.luong.7@gmail.com>
2024-10-21 15:01:30 +0200

    [UI] Added evolution with known type move

--- src/Components/PokemonInfo/EvolutionInfo.js
+++ src/Components/PokemonInfo/EvolutionInfo.js
@@ -1,7 +1,11 @@
 import React from "react";
+
+import { Link } from "react-router-dom";
+
 import usePokemonIds from "../../Hooks/usePokemonIds";
 import useFormat from "../../Hooks/useFormat";
-import { Link } from "react-router-dom";
+
+import TypeInfo from "./TypeInfo";
 
 const EvolutionInfo = ({ data }) => {
     const { getImageFromName } = usePokemonIds();
@@ -16,7 +20,8 @@ const EvolutionInfo = ({ data }) => {
                         <React.Fragment key={i}>
                             <div className="flex flex-col items-center">
                                 {d.min_level && <span>(Level {data.evolution_details[0].min_level})</span>}
-                                {d.min_happiness && <span>(Happy{d.time_of_day ? ` at ${d.time_of_day}` : ""})</span>}
+                                {d.min_happiness && 
+                                    <span>(Happy{d.time_of_day ? ` at ${d.time_of_day}` : ""}{d.known_move_type ? <span> with a {<TypeInfo name={d.known_move_type.name}/>} move</span>: ""})</span>}
                                 {d.item && <>
                                     <img src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/items/${d.item.name}.png`} alt={`Sprite for ${capitalizeAllString(d.item.name)}`} />
                                     <p>{capitalizeAllString(d.item.name)}</p>
