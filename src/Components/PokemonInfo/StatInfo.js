c724595a7f5a24eeaefa41e18dd99aa436277f25
Guy Luong <guy.luong.7@gmail.com>
2024-10-21 23:54:20 +0200

    [UI] Stats: Display bar chart

--- src/Components/PokemonInfo/StatInfo.js
+++ src/Components/PokemonInfo/StatInfo.js
@@ -2,16 +2,17 @@ import useFormat from "../../Hooks/useFormat";
 
 const StatInfo = ({ base, ev, name }) => {
     const { capitalizeAllString } = useFormat();
-    
+
     return (
         <>
-            <div className="flex">
+            <div className="flex items-center">
                 <span className="flex-1">{name === 'hp' ? 'HP' : capitalizeAllString(name)}</span>
-                <span className="flex-1">{base}</span>
+                <span>{base}</span>
+                <div className="flex-3 mx-2 rounded">
+                    <div style={{ width: `${Math.floor(base * 100 / 255)}%` }} className="bg-red-600 h-2 rounded border border-red-500" />
+                </div>
             </div>
-            <div className="w-60">
-                <div style={{ width: `${Math.floor(base * 100 / 255)}%` }} className="bg-red-600" />
-            </div>
+
         </>
     );
 }
