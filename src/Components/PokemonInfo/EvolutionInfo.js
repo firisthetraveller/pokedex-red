115747eca23eb5658204f26cfc691ba3eb0b2776
Guy Luong <guy.luong.7@gmail.com>
2024-10-21 17:39:03 +0200

    [UI] Not evolvable Pokemon added

--- src/Components/PokemonInfo/EvolutionInfo.js
+++ src/Components/PokemonInfo/EvolutionInfo.js
@@ -13,46 +13,51 @@ const EvolutionInfo = ({ data }) => {
 
     return (
         <>
-            {data.evolution_details && data.evolution_details.length > 0 &&
-                <div className="text-center">
-                    <p>{`=>`}</p>
-                    {data.evolution_details.map((d, i) =>
+            {(data.evolution_details.length === 0 && data.evolves_to.length === 0)
+                ? <p>This Pokémon does not evolve.</p>
+                : <>
+                    {data.evolution_details && data.evolution_details.length > 0 &&
+                        <div className="text-center">
+                            <p>{`=>`}</p>
+                            {data.evolution_details.map((d, i) =>
+                                <React.Fragment key={i}>
+                                    <div className="flex flex-col items-center">
+                                        {d.min_level && <span>(Level {data.evolution_details[0].min_level})</span>}
+                                        {d.min_happiness &&
+                                            <span>(Happy{d.time_of_day ? ` at ${d.time_of_day}` : ""}{d.known_move_type ? <span> with a {<TypeInfo name={d.known_move_type.name} />} move</span> : ""})</span>}
+                                        {d.item && <>
+                                            <img src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/items/${d.item.name}.png`} alt={`Sprite for ${capitalizeAllString(d.item.name)}`} />
+                                            <p>{capitalizeAllString(d.item.name)}</p>
+                                        </>}
+                                        {d.location && <>
+                                            <span>Level up at {capitalizeAllString(d.location.name)}</span>
+                                        </>}
+                                    </div>
+                                </React.Fragment>
+                            )}
+                        </div>
+                    }
+                    <div className="text-center">
+                        <Link to={`/pokemon/${data.species.name}`}>
+                            <img src={getImageFromName(data.species.name)} alt={`Sprite of ${data.species.name}`} />
+                            <span className="font-normal">{capitalizeAllString(data.species.name)}</span>
+                        </Link>
+                    </div>
+                    {data.evolves_to && data.evolves_to.length === 1 && data.evolves_to.map((info, i) =>
                         <React.Fragment key={i}>
-                            <div className="flex flex-col items-center">
-                                {d.min_level && <span>(Level {data.evolution_details[0].min_level})</span>}
-                                {d.min_happiness && 
-                                    <span>(Happy{d.time_of_day ? ` at ${d.time_of_day}` : ""}{d.known_move_type ? <span> with a {<TypeInfo name={d.known_move_type.name}/>} move</span>: ""})</span>}
-                                {d.item && <>
-                                    <img src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/items/${d.item.name}.png`} alt={`Sprite for ${capitalizeAllString(d.item.name)}`} />
-                                    <p>{capitalizeAllString(d.item.name)}</p>
-                                </>}
-                                {d.location && <>
-                                    <span>Level up at {capitalizeAllString(d.location.name)}</span>
-                                </>}
-                            </div>
+                            <EvolutionInfo data={info} />
                         </React.Fragment>
                     )}
-                </div>
-            }
-            <div className="text-center">
-                <Link to={`/pokemon/${data.species.name}`}>
-                    <img src={getImageFromName(data.species.name)} alt={`Sprite of ${data.species.name}`} />
-                    <span className="font-normal">{capitalizeAllString(data.species.name)}</span>
-                </Link>
-            </div>
-            {data.evolves_to && data.evolves_to.length === 1 && data.evolves_to.map((info, i) =>
-                <React.Fragment key={i}>
-                    <EvolutionInfo data={info} />
-                </React.Fragment>
-            )}
-            {data.evolves_to && data.evolves_to.length > 1 &&
-                <div>
-                    {data.evolves_to.map((info, i) =>
-                        <div className="flex flex-row items-center" key={i}>
-                            <EvolutionInfo data={info} />
+                    {data.evolves_to && data.evolves_to.length > 1 &&
+                        <div>
+                            {data.evolves_to.map((info, i) =>
+                                <div className="flex flex-row items-center" key={i}>
+                                    <EvolutionInfo data={info} />
+                                </div>
+                            )}
                         </div>
-                    )}
-                </div>
+                    }
+                </>
             }
         </>
     );
--- src/Components/PokemonInfo/EvolutionLine.js
+++ src/Components/PokemonInfo/EvolutionLine.js
@@ -5,7 +5,7 @@ const EvolutionLine = ({ url }) => {
     const { data, error } = useFetch(url);
 
     return (
-        <div className="flex items-center">
+        <div className="flex items-center my-2">
             {data && data.chain && <EvolutionInfo data={data.chain} />}
             {error && <p>{error.message}</p>}
         </div>
