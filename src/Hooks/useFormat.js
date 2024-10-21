2519ba8b928fa4006718499000acced88c3605b0
Guy Luong <guy.luong.7@gmail.com>
2024-10-21 13:40:46 +0200

    [Fix] Added name format exceptions

--- src/Hooks/useFormat.js
+++ src/Hooks/useFormat.js
@@ -1,9 +1,13 @@
+const exceptions = new Map([['nidoran-m', 'Nidoran ♂'], ['nidoran-f', 'Nidoran ♀']]);
+
 const capitalize = (str) => {
     return str.charAt(0).toUpperCase() + str.slice(1);;
 }
 
 const capitalizeAllString = (str) => {
     if (str) {
+        if (exceptions.get(str))
+            return exceptions.get(str);
         return str.split("-").map(s => capitalize(s)).join(" ");
     }
     return undefined;
