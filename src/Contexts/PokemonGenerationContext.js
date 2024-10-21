0c027ac882aa7e96b483baf512079ba8041579b7
Guy Luong <guy.luong.7@gmail.com>
2024-10-21 20:29:59 +0200

    [Dev] Reversed map (id -> games)

--- src/Contexts/PokemonGenerationContext.js
+++ src/Contexts/PokemonGenerationContext.js
@@ -2,6 +2,17 @@ import { createContext, useEffect, useState } from "react";
 
 export const PokemonGenerationContext = createContext(null);
 
+const romanNumerals = {
+    'i': 1, 'ii': 2, 'iii': 3, 'iv': 4,
+    'v': 5, 'vi': 6, 'vii': 7, 'viii': 8,
+    'ix': 9, 'x': 10, 'xi': 11, 'xii': 12
+}
+
+const getGenerationNumber = (name) => {
+    console.log(name);
+    return romanNumerals[name.split('-')[1]]
+}
+
 export const PokemonGenerationProvider = ({ children }) => {
     const [data, setData] = useState(new Map());
     const [errors, setErrors] = useState([]);
@@ -12,7 +23,7 @@ export const PokemonGenerationProvider = ({ children }) => {
             .then((d) => {
                 d.results.forEach(r => {
                     fetch(r.url).then((response) => response.json())
-                        .then(d => setData(m => m.set(r.name, d.generation.name)))
+                        .then(d => setData(m => m.set(getGenerationNumber(d.generation.name), m.get(getGenerationNumber(d.generation.name)) ? m.get(getGenerationNumber(d.generation.name)).add(r.name) : new Set([r.name]))))
                         .catch(err => setErrors(e => [...e, err.message]));
                 });
             })
@@ -22,13 +33,26 @@ export const PokemonGenerationProvider = ({ children }) => {
             });
     }, []);
 
+    const getAllNextGenerations = (start) => {
+        const games = [];
+
+        data.forEach((v, k) => {
+            if (k >= getGenerationNumber(start)) {
+                games.push([...v]);
+            }
+        });
+
+        return games.flat();
+    }
+
     const printAll = () => {
         console.log(data);
     }
 
     return (
-        <PokemonGenerationContext.Provider value={{ ...data, printAll }}>
+        <PokemonGenerationContext.Provider value={{ ...data, printAll, getAllNextGenerations }}>
             {children}
+            {errors && errors.map(e => <p>{e}</p>)}
         </PokemonGenerationContext.Provider>
     )
 }
