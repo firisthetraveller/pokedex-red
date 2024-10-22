adfa0b02fc73613c7d523e1455aba323d98341e9
Guy Luong <guy.luong.7@gmail.com>
2024-10-22 13:16:32 +0200

    [Fix] Warning: map key

--- src/Components/PokemonInfo/StatInfo.js
+++ src/Components/PokemonInfo/StatInfo.js
@@ -8,8 +8,8 @@ const StatInfo = ({ stats }) => {
             {stats && <div className="flex">
                 <div className="flex-1 flex flex-col">
                     {stats.map((s, i) => <>
-                        {/* <StatInfo key={i} base={} ev={s.effort} name={} /> */}
-                        <div className="flex items-center">
+                        <div className="flex items-center" key={i}>
+                            {/* <StatInfo key={i} base={} ev={s.effort} name={} /> */}
                             <span className="flex-1">{s.stat.name === 'hp' ? 'HP' : capitalizeAllString(s.stat.name)}</span>
                             <span className="text-right">{s.base_stat}</span>
                         </div>
@@ -18,7 +18,7 @@ const StatInfo = ({ stats }) => {
 
                 {/** Bars */}
                 <div className="flex-2 lg:flex-3 xl:flex-4 flex flex-col">
-                    {stats.map((s, i) => <div className="flex-1 m-2 rounded">
+                    {stats.map((s, i) => <div className="flex-1 m-2 rounded" key={i}>
                         <div style={{ width: `${Math.floor(s.base_stat * 100 / 255)}%` }} className="bg-red-600 h-2 rounded border border-red-500" />
                     </div>)}
                 </div>
