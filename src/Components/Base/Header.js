a96bb461927ef321c7e324a4c23143087a7b016f
Guy Luong <guy.luong.7@gmail.com>
2024-10-24 23:25:36 +0200

    [Fix] Home link clickable when searching

--- src/Components/Base/Header.js
+++ src/Components/Base/Header.js
@@ -10,11 +10,11 @@ const Header = () => {
     const { width } = useWindowDimensions();
 
     return (
-        <div className="fixed h-24 lg:px-12 pt-4 pl-4 bg-gradient-to-b from-white from-75% z-10 w-full">
+        <div className="fixed flex items-center h-24 lg:px-12 bg-gradient-to-b from-white from-75% z-10 w-full">
             <Link to="/home" className="flex items-center">
-                {width >= 640 && <PokemonLogo />}<Heading level={1} className="ml-2">Pokédex</Heading>
-                <SearchBar className="mx-5"/>
+                {width >= 640 && <PokemonLogo />}<Heading level={1} className="ml-6 sm:ml-2">Pokédex</Heading>
             </Link>
+            <SearchBar className="mx-5"/>
         </div>
     );
 }
