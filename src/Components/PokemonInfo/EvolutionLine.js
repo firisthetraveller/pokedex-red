6c3ac2b3f5b30b4da81b07c54cfee896b0cbc297
Guy Luong <guy.luong.7@gmail.com>
2024-10-25 17:18:12 +0200

    [CSS] Overflow-x scroll for Evolutions

--- src/Components/PokemonInfo/EvolutionLine.js
+++ src/Components/PokemonInfo/EvolutionLine.js
@@ -5,7 +5,7 @@ const EvolutionLine = ({ url }) => {
     const { data, error } = useFetch(url);
 
     return (
-        <div className="flex items-center my-2 max-md:self-center">
+        <div className="flex items-center my-2 max-md:self-center overflow-x-scroll">
             {data && data.chain && <EvolutionInfo data={data.chain} />}
             {error && <p>{error.message}</p>}
         </div>
