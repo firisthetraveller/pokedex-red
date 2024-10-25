951d5c80c8aecc3880fb5dab4bcd8aaa056a1c41
Guy Luong <guy.luong.7@gmail.com>
2024-10-25 17:28:14 +0200

    [Responsive] Updated image size

--- src/Components/PokemonInfo/EvolutionInfo.js
+++ src/Components/PokemonInfo/EvolutionInfo.js
@@ -45,7 +45,7 @@ const EvolutionInfo = ({ data }) => {
                     }
                     <div className="text-center">
                         <Link to={`/pokemon/${data.species.name}`}>
-                            <img src={getImageFromName(data.species.name)} className="lg:h-40 lg:w-40" alt={`Sprite of ${data.species.name}`} />
+                            <img src={getImageFromName(data.species.name)} className="size-24 lg:size-32" alt={`Sprite of ${data.species.name}`} />
                             <span className="font-normal">{capitalizeAllString(data.species.name)}</span>
                         </Link>
                     </div>
--- src/Components/PokemonInfo/VariantInfo.js
+++ src/Components/PokemonInfo/VariantInfo.js
@@ -11,9 +11,9 @@ const VariantInfo = ({ data, name }) => {
         <>
             {
                 displayedData.length > 0
-                    ? displayedData.map((d, i) => <div key={i} className="m-2">
+                    ? displayedData.map((d, i) => <div key={i} className="m-2 w-40">
                         <Link to={`/pokemon/${d.pokemon.name}`} className="flex flex-col items-center">
-                            <img src={getImageFromName(d.pokemon.name)} alt={`Sprite for ${capitalizeAllString(d.pokemon.name)}`} />
+                            <img src={getImageFromName(d.pokemon.name)}  className="size-24 lg:size-32" alt={`Sprite for ${capitalizeAllString(d.pokemon.name)}`} />
                             <p className="font-normal">{capitalizeAllString(d.pokemon.name)}</p>
                         </Link>
                     </div>)
